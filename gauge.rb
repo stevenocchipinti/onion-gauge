36748a8c9e82819ced76bd633efc68f8603316d6
Steve Occhipinti <dev@stevenocchipinti.com>
2017-09-23 13:31:48 +1000

    Move the gauge needle based on an API call
    Using separate repositories for:
    1. Controlling the GPIO pins of the Onion Omega
    2. Serving download usage information from the service provider as JSON
    
    This repository connects the two together!

--- gauge.rb
+++ gauge.rb
@@ -1,8 +1,14 @@
 require "onion_omega"
+require "open-uri"
+require "json"
+
+URL = ENV.fetch("GAUGE_ENDPOINT")
 
 # I think a full revolution is 2048 steps but our gauge is has a smaller range
 stepper = OnionOmega::Stepper.new(
   max_steps: 1566,
   persist_to_file: "current_step"
 )
-stepper.set_percentage 0.0
+
+data = JSON.parse open(URL).read
+stepper.set_percentage data["used_percentage"]
